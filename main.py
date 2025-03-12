67183d0b0ee8b683c313c55621f6cfb500130193
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-03-13 00:20:40 +0100

    feat: add Sample and CalculatedSample classes for data handling

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+class Sample:
+    def __init__(self, dataTag : str, vector : list):
+        self.dataTag = dataTag
+        self.vector = vector
+
+    def __str__(self):
+        return f"Data Tag: {self.dataTag}, Vectors: {self.vector}"
+
+class CalculatedSample:
+    def __init__(self, dataTag : str, vector : list, distance : float):
+        self.dataTag = dataTag
+        self.vector = vector
+        self.distance = distance
+    def __str__(self):
+        return f"Data Tag: {self.dataTag}, Vectors: {self.vector}, Distance: {self.distance}"
+    
 class KNN:
     def __init__(self, k : int, trainingDataPath : str, testDataPath : str):
         self.k = k
