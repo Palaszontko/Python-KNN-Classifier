12e38a09d1cfb3f04c5cd092fc690baa2e972b43
Olgierd Palasz <olgierd.palasz@gmail.com>
2025-03-13 00:19:20 +0100

    feat: add KNN class with initialization parameters

---
+++ main.py
@@ -0,0 +1,6 @@
+class KNN:
+    def __init__(self, k : int, trainingDataPath : str, testDataPath : str):
+        self.k = k
+        self.trainingDataPath = trainingDataPath
+        self.testDataPath = testDataPath
+if __name__ == "__main__":
